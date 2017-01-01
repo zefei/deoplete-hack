cee7df77075620452d9b44949224cf1563a281cc
Zefei Xuan <zefei@fb.com>
2016-12-31 18:32:54 -0800

    moved from vim-hack

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # deoplete-hack
-deoplete.nvim source for Hack
+
+[deoplete](https://github.com/Shougo/deoplete.nvim) source for 
+[Hack](http://hacklang.org/).
+
+## Install
+
+You need to have [Hack 
+setup](https://docs.hhvm.com/hack/getting-started/getting-started) before you 
+can use it inside Vim.
+
+Use your favorite plugin manager and add `zefei/deoplete-hack` to your `.vimrc`:
+
+    Plug 'zefei/deoplete-hack'
+    # or
+    Plugin 'zefei/deoplete-hack'
+    # or
+    NeoBundle 'zefei/deoplete-hack'
+
+## Options
+
+* `g:deoplete#sources#hack#hh_client` (default `'hh_client'`): path to 
+  `hh_client`.
+* `g:deoplete#sources#hack#timeout` (default `0.5`): timeout (in seconds) for 
+  `hh_client` response.
---
+++ rplugin/python3/deoplete/sources/hack.py
@@ -0,0 +1,63 @@
+import json
+import re
+
+from .base import Base
+from subprocess import Popen, PIPE
+from threading import Timer
+
+TOKEN = 'AUTO332'
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+        self.filetypes = ['php']
+        self.input_pattern = r'\w+|[^. \t]->\w*|\w+::\w*|\w\([\'"][^\)]*|\w\(\w*|\\\w*|\$\w*'
+        self.mark = '[Hack]'
+        self.name = 'hack'
+        self.rank = 500
+
+    def on_init(self, context):
+        vars = context['vars']
+        self.hh_client = vars.get('deoplete#sources#hack#hh_client', 'hh_client')
+        self.timeout = vars.get('deoplete#sources#hack#timeout', 0.5)
+
+
+    def get_complete_position(self, context):
+        m = re.search(r'[a-zA-Z_0-9\x7f-\xff\\$]*$', context['input'])
+        return m.start() if m else -1
+
+    def gather_candidates(self, context):
+        [line, column] = self.vim.current.window.cursor
+        line -= 1
+        lines = self.vim.current.buffer[:]
+        lines[line] = lines[line][:column] + TOKEN + lines[line][column:]
+        text = str.encode('\n'.join(lines))
+        command = [
+                self.hh_client,
+                '--auto-complete',
+                '--json',
+                '--from', 'vim',
+                '--retries', '0',
+                '--retry-if-init', 'false',
+                ]
+        try:
+            process = Popen(command, stdout=PIPE, stdin=PIPE)
+            timer = Timer(self.timeout, lambda p: p.kill(), [process])
+            try:
+                timer.start()
+                command_results = process.communicate(input=text)[0]
+                if process.returncode != 0:
+                    return []
+                results = json.loads(command_results.decode('utf-8'))
+                return [parse_result(r) for r in results]
+            finally:
+                timer.cancel()
+        except FileNotFoundError:
+            return []
+
+def parse_result(result):
+    word = result['name']
+    menu = result['type']
+    if menu.startswith('(function'):
+        menu = 'f' + menu[9:-1]
+    return {'word': word, 'menu': menu}
